94abf4f13f28850a86f8238e46cb0dd55466f681
Laura van Nieuwland <lauravannieuwland@gmail.com>
2021-03-20 11:09:33 +0100

    Changed dateformat in javascript so IOS will accept notation as well

--- WiekeDistillers/JS/flipdown.js
+++ WiekeDistillers/JS/flipdown.js
@@ -31,7 +31,7 @@ class FlipDown {
     this.now = this._getTime();
 
     // UTS to count down to
-    this.epoch = new Date('2024.02.21').getTime() / 1000;
+    this.epoch = new Date('2024-02-21').getTime() / 1000;
 
 
     // UTS passed to FlipDown is in the past
